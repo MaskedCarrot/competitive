e079519c1e396bec9888beae44f1c03b2733ba99
MaskedCarrot <2000apoorv@gmail.com>
2020-07-21 20:28:00 +0530

    Add solution for problem `B - Sequential Nim` Link: https://codeforces.com/contest/1382/problem/B Tags: dp, games, *1100 Ref: https://codeforces.com/contest/1382/submission/87543470

---
+++ codeforces/1382/B.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+
+
+
+void solve(){
+	int n;
+	std::cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+		std::cin>>arr[i];
+	int up = 1;
+	int flag = 1;
+	for(int i =0;i<n;i++){
+		if(i == n-1)
+			if(up == 1)
+				std::cout<<"First"<<endl;
+			else
+				std::cout<<"Second"<<endl;
+		if(arr[i] == 1 && flag==1)
+			if(up == 1)
+				up = 2;
+			else 
+				up = 1;
+		else
+		{
+			flag =0;
+		}
+		
+
+	}
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t = 1;
+    int a = 0;
+	cin >> t;
+	while (t--){
+		//std::cout << "Case #" << a+1 <<": ";
+	 	//a++;
+		solve();
+	}
+	return 0;
+}
+
+
+
+
+//               __  __           _            _    ____                     _
+//              |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
+//              | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
+//              | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
+//              |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
+//            =====================================================================
+
+
