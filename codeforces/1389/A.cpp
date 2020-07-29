2cd74bd68a40256c18e936bf35d01d8ba969849d
MaskedCarrot <2000apoorv@gmail.com>
2020-07-29 20:08:00 +0530

    Add solution for problem `A - LCM Problem` Link: https://codeforces.com/contest/1389/problem/A Tags: constructive algorithms, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1389/submission/88298274

---
+++ codeforces/1389/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+
+
+
+void solve(){
+	int x ;
+	int y;
+	std::cin>>x>>y;
+	int a = y/x;
+	if(a <= 1){
+		std::cout<<"-1 -1"<<endl;
+		return;
+	}
+	else{
+		std::cout<<x<<" "<<x*2<<endl;
+	}
+
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t = 1;
+ 	int a = 0;
+	cin >> t;
+	while (t--){
+		//std::cout << "Case #" << a+1 <<": ";
+	 	//a++;
+		solve();
+	}
+	return 0;
+}
+
+
+
+
+//               __  __           _            _    ____                     _
+//              |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
+//              | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
+//              | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
+//              |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
+//            =====================================================================
