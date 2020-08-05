0e1d552d1e4844eb227f589feb652efbb36390e6
MaskedCarrot <2000apoorv@gmail.com>
2020-08-05 20:08:00 +0530

    Add solution for problem `A - Remove Smallest` Link: https://codeforces.com/contest/1399/problem/A Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1399/submission/88957970

--- codeforces/1399/A.cpp
+++ codeforces/1399/A.cpp
@@ -1,42 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define mod 1'000'000'007
 typedef long long ll;
 #define no std::cout << "NO\n"
 #define yes std::cout << "YES\n"
- 
-void solve() {
+
+
+
+void solve(){
 	int n;
-	cin>>n;
+	std::cin>>n;
 	int arr[n];
-	for(int i = 0 ;i<n;i++) 
-		cin>>arr[i];
-	int flag =0;
-	if(n == 1){
-		yes;
-		return;
-	}
+	int flag = 0;
+	for(int i = 0 ;i<n;i++)
+		std::cin>>arr[i];
 	std::sort(arr , arr+n);
-	for(int i =0;i<n-1;i++) {
-		if(arr[i+1] - arr[i] <= 1)
-			continue;
-		else {
+	for(int i = 1 ;i<n;i++){
+		int d = arr[i] - arr[i-1];
+		if(d > 1){
 			flag = 1;
 			break;
 		}
 	}
-	if(flag == 0)
-		yes;
-	else
+	if(flag == 1)
 		no;
- 
- 
+	else
+	{
+		yes;
+	}
+	
 }
- 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+
+int main(){
+	//ios_base::sync_with_stdio(0);
+	//cin.tie(0);
+	cout.tie(0);
 	int t = 1;
     int a = 0;
 	cin >> t;
@@ -47,14 +46,15 @@ int main() {
 	}
 	return 0;
 }
- 
- 
- 
-/*
-*                     __  __           _            _    ____                     _
-*                    |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
-*                    | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
-*                    | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
-*                    |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
-*                  =====================================================================
-*/
+
+
+
+
+//               __  __           _            _    ____                     _
+//              |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
+//              | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
+//              | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
+//              |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
+//            =====================================================================
+
+
