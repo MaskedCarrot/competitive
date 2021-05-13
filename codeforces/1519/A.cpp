e8eebc8bb53bf40a7a0fff70ff6a0a5324130b28
MaskedCarrot <2000apoorv@gmail.com>
2021-05-13 13:17:00 +0530

    Add solution for problem `A - Red and Blue Beans` Link: https://codeforces.com/contest/1519/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1519/submission/116083139

---
+++ codeforces/1519/A.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+     
+    while (y)
+    {
+        if (y % 2 == 1)
+            res = res * x%mod;
+            
+        y = y/2;
+        
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) { 
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+        
+        if (mid * mid == x) {
+            return true;
+        }
+         
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+       
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+void solve() {
+    ll r , b , d;
+    cin>>r>>b>>d;
+    ll m = min(r,b);
+    ll ma = max(r,b);
+    ll p = (d+1)*m;
+    if(ma>p)
+        no;
+    else 
+        yes;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 0;
+    cin >> t;
+    while (t--)
+    {
+        //std::cout << "Case #" << a+1 <<": ";
+        //a++;
+        solve();
+    }
+    return 0;
+}
+
+//                     __  __           _            _    ____                     _
+//                    |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
+//                    | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
+//                    | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
+//                    |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
+//                  =====================================================================
