7b3899c62018749f028f10c4a2c4e3f49b42af02
MaskedCarrot <2000apoorv@gmail.com>
2021-05-15 14:18:00 +0530

    Add solution for problem `B - GCD Length` Link: https://codeforces.com/contest/1511/problem/B Tags: constructive algorithms, math, number theory, *1100 Ref: https://codeforces.com/contest/1511/submission/116244690

---
+++ codeforces/1511/B.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+     
+    while (y)
+    {
+        if (y % 2 == 1)
+            res = res * x%mod;
+            
+        y = y/2;
+        
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) { 
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+        
+        if (mid * mid == x) {
+            return true;
+        }
+         
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+       
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+void solve() {
+    ll a,b,c;
+    cin>>a>>b>>c;
+    ll d1[a] = {0},d2[b] = {0};
+    d1[0] = 1;
+    d2[0] = 1;
+    for(int i =0;i<(b-c+1);++i)
+        d2[i] = 1;
+    for(int i =0;i<a;++i)
+        cout<<d1[i];
+    cout<<' ';
+    for(int i=0;i<b;++i) 
+        cout<<d2[i];
+    cout<<endl;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 0;
+    cin >> t;
+    while (t--)
+    {
+        //std::cout << "Case #" << a+1 <<": ";
+        //a++;
+        solve();
+    }
+    return 0;
+}
+
+//                     __  __           _            _    ____                     _
+//                    |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
+//                    | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
+//                    | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
+//                    |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
+//                  =====================================================================
