496385d55a721a513064feb2ab5296c1b92ca4e2
MaskedCarrot <2000apoorv@gmail.com>
2021-05-16 09:46:00 +0530

    Add solution for problem `C - Game with Chips` Link: https://codeforces.com/contest/1327/problem/C Tags: constructive algorithms, implementation, *1600 Ref: https://codeforces.com/contest/1327/submission/116306055

---
+++ codeforces/1327/C.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+     
+    while (y)
+    {
+        if (y % 2 == 1)
+            res = res * x%mod;
+            
+        y = y/2;
+        
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) { 
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+        
+        if (mid * mid == x) {
+            return true;
+        }
+         
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+       
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+
+long long gcd(long long int a, long long int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+  
+
+void solve() {
+    ll n,m,k;
+    cin>>n>>m>>k;
+    for(int i =0;i<k;++i) {
+        ll x,y;
+        cin>>x>>y;
+    }
+    for(int i =0;i<k;++i) {
+        ll x,y;
+        cin>>x>>y;
+    }
+
+    string res = "";
+    for(int i =0;i<n-1;i++)
+        res += "D";
+    for(int i =0;i<m-1;i++)
+        res += "L";
+    for(int i=0;i<m;i++) {
+        for(int j =0;j<n-1;j++) {
+            if(i%2 == 0) {
+                res += "U";
+            }
+            else {
+                res += "D";
+            }
+        }
+        res += "R";
+    }
+    cout<<res.length()<<endl;
+    cout<<res<<endl;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 0;
+    //cin >> t;
+    while (t--)
+    {
+        //std::cout << "Case #" << a+1 <<": ";
+        //a++;
+        solve();
+    }
+    return 0;
+}
+
+//                     __  __           _            _    ____                     _
+//                    |  \/  | __ _ ___| | _____  __| |  / ___|__ _ _ __ _ __ ___ | |_
+//                    | |\/| |/ _` / __| |/ / _ \/ _` | | |   / _` | '__| '__/ _ \| __|
+//                    | |  | | (_| \__ \   <  __/ (_| | | |__| (_| | |  | | | (_) | |_
+//                    |_|  |_|\__,_|___/_|\_\___|\__,_|  \____\__,_|_|  |_|  \___/ \__|
+//                  =====================================================================
