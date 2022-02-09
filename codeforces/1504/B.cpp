87dffe5ed338c31238e72cdfe7ebfc291788e365
MaskedCarrot <2000apoorv@gmail.com>
2022-02-09 13:16:00 +0530

    Add solution for problem `B - Flip the Bits` Link: https://codeforces.com/contest/1504/problem/B Tags: constructive algorithms, greedy, implementation, math, *1200 Ref: https://codeforces.com/contest/1504/submission/145723844

---
+++ codeforces/1504/B.cpp
@@ -0,0 +1,175 @@
+/*
+
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+                            ```````````````````````````.-:///:-.``````````````
+                            ```````````````````````./-:+o+oooooo/.````````````
+                            ```````````````````````:++ssoooossss/`````````````
+                            `````````````````````..:+oo/-:----:/.`````````````
+                            ``````````````````.-:///++/:-..```````````````````
+                            `````````````````-/++///////++:.``.```````````````
+                            ````````````````:++++////+////++-///:`````````````
+                            ```````````````-yyyysssoosoooooos+::s-```.````````
+                            ````````````....osyyyyyyyyyyyyyys-..+s+//+....````
+                            ``....`````....-++++ooossooooo++:....o+-.`.....```
+                            ``............../////////::--+//-...`..........```
+                            `...............-///+///:---:+/......`..........`.
+                            .................:///////////:....................
+                            ..................-/++++///:......................
+                            ...............-.-+:--:+:..-+/....................
+                            ``````````````.++o.````./://-/:```````````````````
+                            ````````````````.........-.....```````````````````
+                            ``````````````````.............```````````````````
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+
+                     __  __           _            _  ____                     _
+                    |  \/  | __ _ ___| | _____  __| |/ ___|__ _ _ __ _ __ ___ | |_
+                    | |\/| |/ _` / __| |/ / _ \/ _` | |   / _` | '__| '__/ _ \| __|
+                    | |  | | (_| \__ \   <  __/ (_| | |__| (_| | |  | | | (_) | |_
+                    |_|  |_|\__,_|___/_|\_\___|\__,_|\____\__,_|_|  |_|  \___/ \__|
+                  ===================================================================
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+
+//#define TEST
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+    
+    while (y) {
+        if (y % 2 == 1)
+            res = res * x%mod;
+       
+        y = y/2;
+       
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) {
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+       
+        if (mid * mid == x) {
+            return true;
+        }
+       
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+        
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    string str;
+    cin>>str;
+    string str2;
+    cin>>str2;
+
+    if (str == str2) {
+        yes;
+        return;
+    }
+
+    vector<int> v;
+
+    int c =0;
+    for (int i =0;i<str.size();i++) {
+        if (str[i] == '0') c--;
+        else c++;
+
+
+        if (c == 0) v.push_back(i);
+    }
+
+    if (v.size() == 0) {
+        no;
+        return;
+    }
+
+
+    int back = v.back();
+    back++;
+
+    while (back < str.size()) {
+        if (str[back] != str2[back]) {
+            no;
+            return;
+        }
+        back++;
+    } 
+        
+
+    int i =0;
+    int j;
+    int k = 0;
+
+
+
+    while (k < v.size()) {
+        j = v[k];
+        bool flip = (str[i] != str2[i]);
+        for (;i<=j;i++) {
+            if (flip) {
+                if (str[i] == str2[i]) {
+                    no;
+                    return;
+                }
+            } else {
+                if (str[i] != str2[i]) {
+                    no;
+                    return;
+                }
+            }
+        }
+        k++;
+        
+    }
+
+    yes;
+}
+
+
+int main() {
+    #ifdef TEST
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("errors.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 1;
+    cin >> t;
+    while (t--) {
+        //std::cout << "Case #"<<a<<": ";
+        a++;
+        solve();
+    }
+    return 0;
+}
