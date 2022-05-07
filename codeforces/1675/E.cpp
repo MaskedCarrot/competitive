74f609aae635bcc1ef7beda5652eafa885a3b5a7
MaskedCarrot <2000apoorv@gmail.com>
2022-05-07 20:06:00 +0530

    Add solution for problem `E - Replace With the Previous, Minimize` Link: https://codeforces.com/contest/1675/problem/E Tags: dsu, greedy, strings, *1500 Ref: https://codeforces.com/contest/1675/submission/156207269

---
+++ codeforces/1675/E.cpp
@@ -0,0 +1,163 @@
+/*
+
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+                            ```````````````````````````.-:///:-.``````````````
+                            ```````````````````````./-:+o+oooooo/.````````````
+                            ```````````````````````:++ssoooossss/`````````````
+                            `````````````````````..:+oo/-:----:/.`````````````
+                            ``````````````````.-:///++/:-..```````````````````
+                            `````````````````-/++///////++:.``.```````````````
+                            ````````````````:++++////+////++-///:`````````````
+                            ```````````````-yyyysssoosoooooos+::s-```.````````
+                            ````````````....osyyyyyyyyyyyyyys-..+s+//+....````
+                            ``....`````....-++++ooossooooo++:....o+-.`.....```
+                            ``............../////////::--+//-...`..........```
+                            `...............-///+///:---:+/......`..........`.
+                            .................:///////////:....................
+                            ..................-/++++///:......................
+                            ...............-.-+:--:+:..-+/....................
+                            ``````````````.++o.````./://-/:```````````````````
+                            ````````````````.........-.....```````````````````
+                            ``````````````````.............```````````````````
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+
+                     __  __           _            _  ____                     _
+                    |  \/  | __ _ ___| | _____  __| |/ ___|__ _ _ __ _ __ ___ | |_
+                    | |\/| |/ _` / __| |/ / _ \/ _` | |   / _` | '__| '__/ _ \| __|
+                    | |  | | (_| \__ \   <  __/ (_| | |__| (_| | |  | | | (_) | |_
+                    |_|  |_|\__,_|___/_|\_\___|\__,_|\____\__,_|_|  |_|  \___/ \__|
+                  ===================================================================
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+#define vin(v, n) for (ll i = 0;i<n;i++) { ll x; cin>>x; v.push_back(x); }
+#define all(x) x.begin(), x.end()
+
+//#define TEST
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+    
+    while (y) {
+        if (y % 2 == 1)
+            res = res * x%mod;
+
+        y = y/2;
+
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) {
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+       
+        if (mid * mid == x) {
+            return true;
+        }
+       
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+        
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+
+void _union(vector<char> &parent, char new_char, char old_char) {
+    parent[old_char - 'a'] = new_char;
+}
+
+char find(vector<char> &parent, char x) {
+    return (parent[x-'a'] == x)? x:find(parent, parent[x-'a']);
+}
+
+void solve() {
+    int n, k;
+    cin>>n>>k;
+    string str;
+    cin>>str;
+
+    vector<char> parent(26);
+    for (int i=0;i<26;i++) {
+        parent[i] = (char)(i+'a');
+    }
+
+    vector<int> freq(26, 0);
+    for (int i=0;i<n;i++) {
+        freq[str[i]-'a']++;
+    }
+
+    set<char> proc;
+    proc.insert('a');
+    int i = 0;
+
+    while (i < n) {
+        char ch = find(parent, str[i]);
+        if (proc.find(ch) == proc.end()) {
+            proc.insert(ch);
+            if (k > 0) {
+                k--;
+                char new_char = (char)(ch-1);
+                freq[new_char-'a'] += freq[ch-'a'];
+                _union(parent, new_char, ch);
+                //cout<<"modifying "<<ch<<" to "<<new_char<<endl;
+            } else {
+                break;
+            }
+        } else {
+            i++;
+        }
+    }
+
+    // for (int i=0;i<26;i++) {
+    //     cout<<(char)('a'+i)<<" = "<<find(parent, (char)('a'+i))<<endl;
+    // }
+
+    for (int i=0;i<n;i++) {
+        cout<<find(parent, str[i]);
+    }
+    cout<<endl;
+
+}
+
+int main() {
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("errors.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 1;
+    cin >> t;
+    while (t--) {
+        //std::cout << "Case #"<<a<<": ";
+        a++;
+        solve();
+    }
+    return 0;
+}
