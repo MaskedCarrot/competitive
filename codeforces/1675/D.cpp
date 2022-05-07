07e87b901d4cd7d70ab2f06bfd0c353fa7931757
MaskedCarrot <2000apoorv@gmail.com>
2022-05-07 19:29:00 +0530

    Add solution for problem `D - Vertical Paths` Link: https://codeforces.com/contest/1675/problem/D Tags: graphs, implementation, trees, *1300 Ref: https://codeforces.com/contest/1675/submission/156204077

---
+++ codeforces/1675/D.cpp
@@ -0,0 +1,150 @@
+/*
+
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+                            ```````````````````````````.-:///:-.``````````````
+                            ```````````````````````./-:+o+oooooo/.````````````
+                            ```````````````````````:++ssoooossss/`````````````
+                            `````````````````````..:+oo/-:----:/.`````````````
+                            ``````````````````.-:///++/:-..```````````````````
+                            `````````````````-/++///////++:.``.```````````````
+                            ````````````````:++++////+////++-///:`````````````
+                            ```````````````-yyyysssoosoooooos+::s-```.````````
+                            ````````````....osyyyyyyyyyyyyyys-..+s+//+....````
+                            ``....`````....-++++ooossooooo++:....o+-.`.....```
+                            ``............../////////::--+//-...`..........```
+                            `...............-///+///:---:+/......`..........`.
+                            .................:///////////:....................
+                            ..................-/++++///:......................
+                            ...............-.-+:--:+:..-+/....................
+                            ``````````````.++o.````./://-/:```````````````````
+                            ````````````````.........-.....```````````````````
+                            ``````````````````.............```````````````````
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+
+                     __  __           _            _  ____                     _
+                    |  \/  | __ _ ___| | _____  __| |/ ___|__ _ _ __ _ __ ___ | |_
+                    | |\/| |/ _` / __| |/ / _ \/ _` | |   / _` | '__| '__/ _ \| __|
+                    | |  | | (_| \__ \   <  __/ (_| | |__| (_| | |  | | | (_) | |_
+                    |_|  |_|\__,_|___/_|\_\___|\__,_|\____\__,_|_|  |_|  \___/ \__|
+                  ===================================================================
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+#define vin(v, n) for (ll i = 0;i<n;i++) { ll x; cin>>x; v.push_back(x); }
+#define all(x) x.begin(), x.end()
+
+//#define TEST
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+    
+    while (y) {
+        if (y % 2 == 1)
+            res = res * x%mod;
+
+        y = y/2;
+
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) {
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+       
+        if (mid * mid == x) {
+            return true;
+        }
+       
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+        
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+
+vector<vector<int>> ans;
+
+void helper(map<int,vector<int>> &m, int x) {
+    //cout<<"x = "<<x<<' ';
+    ans.back().push_back(x);
+    for (int i =0;i<m[x].size();i++) {
+        helper(m, m[x][i]);
+        if (i+1 < m[x].size()) ans.push_back({});
+    }
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    map<int,vector<int>> m;
+    ans.clear();
+    int start;
+
+    for (int i=0;i<n;i++) {
+        int x;
+        cin>>x;
+        if (x == i+1) {
+            start = i+1;
+        } else {
+            m[x].push_back(i+1);
+        }
+        
+    }
+
+    ans.push_back({});
+    helper(m, start);
+
+    cout<<ans.size()<<endl;
+    for (int i=0;i<ans.size();i++) {
+        cout<<ans[i].size()<<endl;
+        for (int j=0;j<ans[i].size();j++) {
+            cout<<ans[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+}
+
+int main() {
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("errors.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 1;
+    cin >> t;
+    while (t--) {
+        //std::cout << "Case #"<<a<<": ";
+        a++;
+        solve();
+    }
+    return 0;
+}
