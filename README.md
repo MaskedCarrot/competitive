b27a44fedff0f1fb84f9285868efe44eb1aedb10
MaskedCarrot <2000apoorv@gmail.com>
2022-05-10 21:37:00 +0530

    Add solution for problem `F - Longest Strike` Link: https://codeforces.com/contest/1676/problem/F Tags: data structures, greedy, implementation, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/1676/submission/156715658

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-6 | [A - Prof. Slim](https://codeforces.com/contest/1670/problem/A) | [GNU C++17](./codeforces/1670/A.cpp) | `greedy` `implementation` `sortings` `*800` | May/22/2022 12:00 | 
-5 | [B - Stone Age Problem](https://codeforces.com/contest/1679/problem/B) | [GNU C++17](./codeforces/1679/B.cpp) | `data structures` `implementation` `*1200` | May/14/2022 16:13 | 
-4 | [A - AvtoBus](https://codeforces.com/contest/1679/problem/A) | [GNU C++17](./codeforces/1679/A.cpp) | `brute force` `greedy` `math` `number theory` `*900` | May/14/2022 15:43 | 
-3 | [B - Robots](https://codeforces.com/contest/1680/problem/B) | [GNU C++17](./codeforces/1680/B.cpp) | `implementation` `*800` | May/13/2022 20:24 | 
-2 | [A - Minimums and Maximums](https://codeforces.com/contest/1680/problem/A) | [GNU C++17](./codeforces/1680/A.cpp) | `brute force` `math` `*800` | May/13/2022 20:15 | 
-1 | [D - X-Sum](https://codeforces.com/contest/1676/problem/D) | [GNU C++17](./codeforces/1676/D.cpp) | `brute force` `greedy` `implementation` `*1000` | May/11/2022 14:16 | 
+7 | [A - Prof. Slim](https://codeforces.com/contest/1670/problem/A) | [GNU C++17](./codeforces/1670/A.cpp) | `greedy` `implementation` `sortings` `*800` | May/22/2022 12:00 | 
+6 | [B - Stone Age Problem](https://codeforces.com/contest/1679/problem/B) | [GNU C++17](./codeforces/1679/B.cpp) | `data structures` `implementation` `*1200` | May/14/2022 16:13 | 
+5 | [A - AvtoBus](https://codeforces.com/contest/1679/problem/A) | [GNU C++17](./codeforces/1679/A.cpp) | `brute force` `greedy` `math` `number theory` `*900` | May/14/2022 15:43 | 
+4 | [B - Robots](https://codeforces.com/contest/1680/problem/B) | [GNU C++17](./codeforces/1680/B.cpp) | `implementation` `*800` | May/13/2022 20:24 | 
+3 | [A - Minimums and Maximums](https://codeforces.com/contest/1680/problem/A) | [GNU C++17](./codeforces/1680/A.cpp) | `brute force` `math` `*800` | May/13/2022 20:15 | 
+2 | [D - X-Sum](https://codeforces.com/contest/1676/problem/D) | [GNU C++17](./codeforces/1676/D.cpp) | `brute force` `greedy` `implementation` `*1000` | May/11/2022 14:16 | 
+1 | [F - Longest Strike](https://codeforces.com/contest/1676/problem/F) | [GNU C++17](./codeforces/1676/F.cpp) | `data structures` `greedy` `implementation` `sortings` `two pointers` `*1300` | May/10/2022 21:37 | 
---
+++ codeforces/1676/F.cpp
@@ -0,0 +1,162 @@
+/*
+
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+                            ```````````````````````````.-:///:-.``````````````
+                            ```````````````````````./-:+o+oooooo/.````````````
+                            ```````````````````````:++ssoooossss/`````````````
+                            `````````````````````..:+oo/-:----:/.`````````````
+                            ``````````````````.-:///++/:-..```````````````````
+                            `````````````````-/++///////++:.``.```````````````
+                            ````````````````:++++////+////++-///:`````````````
+                            ```````````````-yyyysssoosoooooos+::s-```.````````
+                            ````````````....osyyyyyyyyyyyyyys-..+s+//+....````
+                            ``....`````....-++++ooossooooo++:....o+-.`.....```
+                            ``............../////////::--+//-...`..........```
+                            `...............-///+///:---:+/......`..........`.
+                            .................:///////////:....................
+                            ..................-/++++///:......................
+                            ...............-.-+:--:+:..-+/....................
+                            ``````````````.++o.````./://-/:```````````````````
+                            ````````````````.........-.....```````````````````
+                            ``````````````````.............```````````````````
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+
+                     __  __           _            _  ____                     _
+                    |  \/  | __ _ ___| | _____  __| |/ ___|__ _ _ __ _ __ ___ | |_
+                    | |\/| |/ _` / __| |/ / _ \/ _` | |   / _` | '__| '__/ _ \| __|
+                    | |  | | (_| \__ \   <  __/ (_| | |__| (_| | |  | | | (_) | |_
+                    |_|  |_|\__,_|___/_|\_\___|\__,_|\____\__,_|_|  |_|  \___/ \__|
+                  ===================================================================
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+#define vin(v, n) for (ll i = 0;i<n;i++) { ll x; cin>>x; v.push_back(x); }
+#define all(x) x.begin(), x.end()
+
+//#define TEST
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+    
+    while (y) {
+        if (y % 2 == 1)
+            res = res * x%mod;
+
+        y = y/2;
+
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) {
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+       
+        if (mid * mid == x) {
+            return true;
+        }
+       
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+        
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    map<ll,ll> m;
+    vector<ll> v;
+    for (int i=0;i<n;i++) {
+        ll x;
+        cin>>x;
+        if (m[x] == 0) v.push_back(x);
+        m[x]++;
+    }
+
+    sort(all(v));
+
+    ll len = v.size();
+
+    vector<bool> dp(len, false);
+
+    for (ll i=0;i<len;i++) {
+        dp[i] = ((m[v[i]] >= k));
+    }
+
+    ll tl = INT_MAX;
+    ll tr = INT_MIN;
+    ll l = tl;
+    ll r = tr;
+    for (int i=0;i<len;i++) {
+        if (i > 0 && v[i]-v[i-1] > 1) {
+            tl = INT_MAX;
+            tr = INT_MIN;
+        }
+        if (dp[i]) {
+            if (tl == INT_MAX) tl = v[i];
+            tr = v[i];
+        } else {
+            tl = INT_MAX;
+            tr = INT_MIN;
+        }
+        if (tr-tl+1 > r-l+1) {
+            r = tr;
+            l = tl;
+        }
+    }
+
+    if (tr-tl+1 > r-l+1) {
+            r = tr;
+            l = tl;
+    }
+
+    if (l == INT_MAX) cout<<-1<<endl;
+    else cout<<l<<' '<<r<<endl;
+    
+}
+
+int main() {
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("errors.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 1;
+    cin >> t;
+    while (t--) {
+        //std::cout << "Case #"<<a<<": ";
+        a++;
+        solve();
+    }
+    return 0;
+}
