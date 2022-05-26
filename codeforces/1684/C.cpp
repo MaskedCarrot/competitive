60c58dd4c7d13075530fc5ce01eef96e830724d8
MaskedCarrot <2000apoorv@gmail.com>
2022-05-26 13:13:00 +0530

    Add solution for problem `C - Column Swapping` Link: https://codeforces.com/contest/1684/problem/C Tags: brute force, constructive algorithms, greedy, implementation, sortings, *1400 Ref: https://codeforces.com/contest/1684/submission/158502739

---
+++ codeforces/1684/C.cpp
@@ -0,0 +1,150 @@
+/*
+
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+                            ```````````````````````````.-:///:-.``````````````
+                            ```````````````````````./-:+o+oooooo/.````````````
+                            ```````````````````````:++ssoooossss/`````````````
+                            `````````````````````..:+oo/-:----:/.`````````````
+                            ``````````````````.-:///++/:-..```````````````````
+                            `````````````````-/++///////++:.``.```````````````
+                            ````````````````:++++////+////++-///:`````````````
+                            ```````````````-yyyysssoosoooooos+::s-```.````````
+                            ````````````....osyyyyyyyyyyyyyys-..+s+//+....````
+                            ``....`````....-++++ooossooooo++:....o+-.`.....```
+                            ``............../////////::--+//-...`..........```
+                            `...............-///+///:---:+/......`..........`.
+                            .................:///////////:....................
+                            ..................-/++++///:......................
+                            ...............-.-+:--:+:..-+/....................
+                            ``````````````.++o.````./://-/:```````````````````
+                            ````````````````.........-.....```````````````````
+                            ``````````````````.............```````````````````
+                            ``````````````````````````````````````````````````
+                            ``````````````````````````````````````````````````
+
+                     __  __           _            _  ____                     _
+                    |  \/  | __ _ ___| | _____  __| |/ ___|__ _ _ __ _ __ ___ | |_
+                    | |\/| |/ _` / __| |/ / _ \/ _` | |   / _` | '__| '__/ _ \| __|
+                    | |  | | (_| \__ \   <  __/ (_| | |__| (_| | |  | | | (_) | |_
+                    |_|  |_|\__,_|___/_|\_\___|\__,_|\____\__,_|_|  |_|  \___/ \__|
+                  ===================================================================
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1'000'000'007
+typedef long long int ll;
+#define no std::cout << "NO\n"
+#define yes std::cout << "YES\n"
+#define vin(v, n) for (ll i = 0;i<n;i++) { ll x; cin>>x; v.push_back(x); }
+#define all(x) x.begin(), x.end()
+
+//#define TEST
+
+
+ll power(ll x, ll y) {
+    ll res = 1;
+    
+    while (y) {
+        if (y % 2 == 1)
+            res = res * x%mod;
+
+        y = y/2;
+
+        x = (x * x)%mod;
+    }
+    return res;
+}
+
+bool isPerfectSquare(ll x) {
+    ll left = 1, right = x;
+   
+    while (left <= right) {
+        ll mid = (left + right) / 2;
+       
+        if (mid * mid == x) {
+            return true;
+        }
+       
+        if (mid * mid < x) {
+            left = mid + 1;
+        }
+        
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / __gcd(a, b)) * b;
+}
+
+void solve() {
+    int n, m;
+    cin>>n>>m;
+
+    vector<vector<int>> v(n, vector<int>(m));
+
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            cin>>v[i][j];
+        }
+    }
+
+    vector<int> bad;
+
+    for (int i=0;i<n && bad.size() == 0;i++) {
+        vector<int> a = v[i];
+        sort(all(a));
+        for (int j =0;j<m;j++) {
+            if (a[j] != v[i][j]) bad.push_back(j);
+        }
+    }
+
+    if (bad.size() > 2) {
+        cout<<-1<<endl;
+    } else if (bad.size() == 0) {
+        cout<<1<<' '<<1<<endl;
+    } else {
+        for (int i=0;i<n;i++) {
+            swap(v[i][bad[0]], v[i][bad[1]]);
+        }
+
+        for (int i =0;i<n;i++) {
+            for (int j=0;j<m-1;j++) {
+                if (v[i][j] > v[i][j+1]) {
+                    cout<<-1<<endl;
+                    return;
+                }
+            }
+        }
+
+        cout<<bad[0]+1<<' '<<bad[1]+1<<endl;
+    }
+}
+
+int main() {
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("errors.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    ll a = 1;
+    cin >> t;
+    while (t--) {
+        //std::cout << "Case #"<<a<<": ";
+        a++;
+        solve();
+    }
+    return 0;
+}
